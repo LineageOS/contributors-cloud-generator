75be0d9ced0c5e2f8ca71a46ebfe25164aed9ca0
Harry Youd <harry@harryyoud.co.uk>
2018-02-24 23:22:14 +0000

    Rewrite script in python
    Change-Id: Id657e21fe7e4c247ac586cf8c943d7138feba42b

---
+++ generate_wordcloud.py
@@ -0,0 +1,71 @@
+from pygerrit2.rest import GerritRestAPI
+from requests.auth import HTTPBasicAuth
+import json
+import os
+import git
+from subprocess import call
+import concurrent.futures
+import argparse
+
+gerrit = GerritRestAPI(url="https://review.lineageos.org")
+
+accounts_dir = 'db/accounts'
+projects_dir = 'db/projects'
+stats_dir = 'db/stats'
+out_dir = 'out'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mirror', metavar='mirror_dir', type=str,
+                    help='path to mirror repo')
+args = parser.parse_args()
+
+directories = [accounts_dir, projects_dir, stats_dir, out_dir]
+for directory in directories:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+repeat = True
+accounts = []
+while repeat:
+    accounts.extend(gerrit.get('/accounts/?q=-name:""&n=20&S={}'.format(len(accounts))))
+    if '_more_accounts' not in accounts[-1]:
+        repeat = False
+for account in accounts:
+    print('Fetching account {}'.format(account['_account_id']))
+    file = open("{}/{}".format(accounts_dir, account['_account_id']), "w")
+    file.write(")]}'\n")
+    file.write(json.dumps(gerrit.get('/accounts/{}'.format(account['_account_id'])), indent=2))
+    file.write('\n')
+    file.close()
+
+def repo_fetch(project):
+    project_stats_dir = os.path.join(stats_dir,project)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    repodir = os.path.join(projects_dir,project,'.git')
+    if os.path.exists(repodir):
+        call(['git', '-C', repodir, 'fetch', 'origin'])
+    else:
+        os.makedirs(repodir)
+        if args.reference:
+            call(['git', 'clone', '--no-checkout', '--reference', os.path.join(args.reference+project), 'https://github.com/'+project, project])
+        else:
+            call(['git', 'clone', '--no-checkout', 'https://github.com/'+project, project])
+
+    f = open(project_stats_dir+'/all_stats.dat', 'w')
+    call(['git', '-C', repodir, 'shortlog', '-esn', '--all'], stdout=f)
+    f.close()
+    f = open(project_stats_dir+'/translations_stats.dat', 'w')
+    call(['git', '-C', repodir, 'shortlog', '-esn', '--all', '--grep="Automatic translation import"'], stdout=f)
+    f.close()
+
+projects = gerrit.get('/projects/?p=LineageOS')
+executor = concurrent.futures.ProcessPoolExecutor(8)
+futures = [executor.submit(repo_fetch, project) for project in projects]
+concurrent.futures.wait(futures)
+
+call(['java', '-Dfile.encoding=UTF-8', '-classpath', '"./lib/*"', 'CloudGenerator'])
+
+print("====================================")
+print("== Cloud generated: {}/cloud.zip ==").format(out_dir)
+print("====================================")
