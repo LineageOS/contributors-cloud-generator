9fa6f748c8dab87a4e6e8b6a97798865f3266d5d
Stricted <info@stricted.net>
2018-09-16 01:36:44 +0200

    clone as bare repository
    Change-Id: Id5e3492c772558a9a87dab66a81f8163a341312a

--- repo_fetch.sh
+++ repo_fetch.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
 GIT_CMD=/usr/bin/git
-GIT_USER=a
-GIT_PWD=a
 
 OLD_PWD=`pwd`
-REPO_DIR=$OLD_PWD/db/repos/$1
+REPO_DIR=$OLD_PWD/db/repos/$1.git
 STATS_DIR=$OLD_PWD/db/stats/$1
 
 echo "REPO: $REPO_DIR"
 if [ ! -d "$REPO_DIR" ]; then
-    mkdir -p $REPO_DIR
-    cd $REPO_DIR
-    $GIT_CMD init
-    $GIT_CMD remote add origin https://$GIT_USER:$GIT_PWD@github.com/$1.git
-    $GIT_CMD fetch
-else
-    cd $REPO_DIR
-    $GIT_CMD fetch
+    $GIT_CMD clone --bare https://github.com/$1.git $REPO_DIR
 fi
+$GIT_CMD --bare --git-dir=$REPO_DIR fetch -p origin '+refs/heads/*:refs/heads/*'
 
 # Get repository stats
 mkdir -p $STATS_DIR
-$GIT_CMD shortlog -e -s -n --all > $STATS_DIR/all_stats.dat
-$GIT_CMD shortlog -e -s -n --all --grep='Automatic translation import' > $STATS_DIR/translations_stats.dat
+$GIT_CMD --bare --git-dir=$REPO_DIR shortlog -e -s -n --all > $STATS_DIR/all_stats.dat
+$GIT_CMD --bare --git-dir=$REPO_DIR shortlog -e -s -n --all --grep='Automatic translation import' > $STATS_DIR/translations_stats.dat
 
 cd $OLD_PWD
