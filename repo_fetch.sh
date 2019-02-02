63ae5789bd5ca1d71aacb3d22f31b6265f0c6697
Tim Schumacher <timschumi@gmx.de>
2019-02-02 14:52:39 +0100

    Add support for local mirrors
    Change-Id: If1d98b81bc10d4ab4837a976603498f88e124a08

--- repo_fetch.sh
+++ repo_fetch.sh
@@ -8,14 +8,20 @@ OLD_PWD=`pwd`
 REPO_DIR=$OLD_PWD/db/repos/$1
 STATS_DIR=$OLD_PWD/db/stats/$1
 
-echo "REPO: $REPO_DIR"
-if [ ! -d "$REPO_DIR" ]; then
+MIRROR_DIR=/path/to/mirror/$1.git
+
+if [ -d "$MIRROR_DIR" ]; then
+    echo "MIRROR: $MIRROR_DIR"
+    cd "$MIRROR_DIR"
+elif [ ! -d "$REPO_DIR" ]; then
+    echo "REPO: $REPO_DIR"
     mkdir -p $REPO_DIR
     cd $REPO_DIR
     $GIT_CMD init
     $GIT_CMD remote add origin https://$GIT_USER:$GIT_PWD@github.com/$1.git
     $GIT_CMD fetch
 else
+    echo "REPO: $REPO_DIR"
     cd $REPO_DIR
     $GIT_CMD fetch
 fi
